6e0e609f58a8e71142c31c03b1ac1fe7d57f84ba
Saira Syed <sairasyed111@gmail.com>
2016-02-09 15:36:16 -0800

    complete divisor?

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,22 +2,67 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
+  (if (or (= x nil)
+          (= x false))
+    false
+    true))
+
+(boolean "foo")   ;=> true
+(boolean nil)     ;=> false
+
+; Exercise 1
+(boolean "foo") ;=> true
+(boolean nil) ;=> false
+(boolean (+ 2 3)) ;=> true
+(boolean true) ;=> true
+(boolean false) ;=> false
+
+
+; Exercise 3
 
 (defn abs [x]
-  ":(")
+  (if (< x 0)
+    (- x)
+    x ))
 
-(defn divides? [divisor n]
-  ":(")
+(abs -2)
+
+
+; Exercise 4
+(defn divides? [divisor  n]
+  (= 0 (mod n divisor)))
+
+(divides? 2 4) ;=> true
+(divides? 4 2) ;=> false
+(divides? 5 10) ;=> true
+(divides? 2 5) ;=> false
 
 (defn fizzbuzz [n]
   ":(")
 
+; Exercise 2
+
+
 (defn teen? [age]
-  ":(")
+  (if (and (<= age 19)
+          (>= age 13))
+      true
+      false) )
+(teen? 12) ;=> false
+(teen? 15) ;=> true
+(teen? 19) ;=> true
+(teen? 20) ;=> false
+(teen? 27) ;=> false
+
 
 (defn not-teen? [age]
-  ":(")
+  (if (or (>= age 19)
+           (<= age 13))
+    true
+    false ))
+
+(not-teen? 10)
+(not-teen? 20)
 
 (defn generic-doublificate [x]
   ":(")
@@ -26,3 +71,5 @@
   ":(")
 
 ; '_______'
+
+
